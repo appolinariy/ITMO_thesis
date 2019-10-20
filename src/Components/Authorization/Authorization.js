59ae8a52b53a110e8432d36ddc231dbc2f6bb3fb
appolinariy <abramova.polina.2001@mail.ru>
2019-10-20 13:07:50 +0300

    Made layout

--- src/Components/Authorization/Authorization.js
+++ src/Components/Authorization/Authorization.js
@@ -2,45 +2,45 @@ import React, { Component } from 'react';
 import './Authorization.css';
 
 class Authorization extends Component{
-    state = {
-        values: {
-            login: 'abramova',
-            password: '12345'
-        }
-    };
-
-    handleChange = event => {
-        this.setState({
-            values: {
-                ...this.state.values,
-                [event.target.login]: event.target.value,
-                [event.target.password]: event.target.value
-            }
-        });
+  state = {
+    values: {
+      login: '',
+      password: ''
     }
+  };
 
-    handleSubmit = event => {
-        event.preventDefault();
-        console.log(this.state.values.login);
-        console.log(this.state.values.password);
-    }
+  handleChange = event => {
+    this.setState({
+      values: {
+        ...this.state.values,
+        [event.target.name]: event.target.value,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
 
-    render(){
-        return(
-            <div className='mainblock'>
-                <h2>Авторизация</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Логин: 
-                    <input type='text' name='login' value={this.state.values.login} onChange={this.handleChange} />
-                    </label>
-                    <label>Пароль: 
-                    <input type='text' name='password' value={this.state.values.password} onChange={this.handleChange} />
-                    </label>
-                    <input type='submit' value='Print' />
-                </form>
-            </div>
-        );
-    }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.values.login);
+    console.log(this.state.values.password);
+  }
+
+  render(){
+    return(
+      <main>
+        <div className='mainblock'>
+          <h3>Авторизуйтесь в системе</h3>
+          <form onSubmit={this.handleSubmit}>
+            <label>Логин: </label>
+              <input type='text' name='login' placeholder='Введите логин' value={this.state.values.login} onChange={this.handleChange} />
+            <label>Пароль:</label>
+              <input type='text' name='password' placeholder='Введите пароль' value={this.state.values.password} onChange={this.handleChange} />
+            <input type='submit' value='Войти' />
+          </form>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Authorization;
