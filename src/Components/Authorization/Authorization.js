d84561d3527bfe46e92599cb9c1c0a128b7daf2d
appolinariy <abramova.polina.2001@mail.ru>
2019-11-04 20:40:06 +0300

    Condition render

--- src/Components/Authorization/Authorization.js
+++ src/Components/Authorization/Authorization.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import './Authorization.css';
 
+
+
 class Authorization extends Component{
   state = {
     values: {
       login: '',
       password: ''
-    }
+    },
+    button: false
   };
 
   handleChange = event => {
     this.setState({
       values: {
         ...this.state.values,
-        [event.target.name]: event.target.value,
         [event.target.name]: event.target.value
       }
     });
@@ -21,11 +23,19 @@ class Authorization extends Component{
 
   handleSubmit = event => {
     event.preventDefault();
+    if((this.state.values.login==='polina')&&(this.state.values.password==='12345')){
+      this.setState({
+      button: !this.state.button
+      })
+    }
+    console.log(this.state.button);
     console.log(this.state.values.login);
     console.log(this.state.values.password);
   }
 
+
   render(){
+    let content = (this.state.button===true) && <h1>Hello</h1>;
     return(
       <main>
         <div className='mainblock'>
@@ -38,6 +48,7 @@ class Authorization extends Component{
             <input type='submit' value='Войти' />
           </form>
         </div>
+        {content}
       </main>
     );
   }
